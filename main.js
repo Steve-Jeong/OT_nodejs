0fbaeb33d7e85a42ef3b497dcf156c5b57ae3670
Steve Jeong <jst0930@gmail.com>
2020-09-05 00:02:10 +0900

    Basic Homepage completed

--- main.js
+++ main.js
@@ -1,27 +1,76 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url')
-
-// var no_cycle=1;
+var no_cycle = 1;
+var template = null;
 var app = http.createServer(function (request, response) {
-  // console.log('########### number of cycle : '+(no_cycle++)+'  ###########')
-  // console.log('1. request.url : ', request.url)
-  var _url = url.parse(request.url);
-  // console.log('2. parsed url : ', _url);
-  // console.log('2-1. query : ', _url.query)
-  if (_url.pathname == '/') {
-    _url = '/index.html';
-    // console.log('3. inside if', _url)
+  //   console.log('########### number of cycle : '+(no_cycle++)+'  ###########')
+  //   console.log('1. request.url : ', request.url)
+  var _url = url.parse(request.url, true);
+  //   console.log('2. parsed url : ', _url);
+  //   console.log('2-1. query : ', _url.query)
+  var title = _url.query.id;
+  var pathname = _url.pathname;
+  if (pathname === '/') {
+    // console.log('3. inside if', pathname)
+    if(title=== undefined) {
+      title = 'Welcome';
+      description = 'Hello NodeJS';
+      template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+      `;
+      response.writeHead(200);
+      response.end(template);
+    } else {
+      fs.readFile(`data/${title}`, 'utf8', (err, description) => {
+        // console.log('title : ', title);
+        // console.log('description : ', description);
+        template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+        `;
+        response.writeHead(200);
+        response.end(template);
+      })
+    }
+  } else {
+    response.writeHead(404);
+    response.end('<h1><a href="/">WEB</a></h1><p>Page Not Found.</p>');
   }
-  if (_url.pathname == '/favicon.ico') {
-    // console.log('4. inside favicon.ico')
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  // console.log('5. dirname : ', __dirname);
-  // console.log('6. url after if: ', _url.pathname);
-  response.end(fs.readFileSync(__dirname + _url));
-  // response.end(__dirname);
+  //   console.log('5. dirname : ', __dirname);
+  //   console.log('6. url after if: ', _url.pathname);
+
 
 });
 
---
+++ readfile.js
@@ -0,0 +1,5 @@
+fs = require('fs')
+fs.readFile('/etc/passwd', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
