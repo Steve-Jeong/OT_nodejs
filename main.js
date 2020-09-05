13523730cc293ea2301fa789487a0efec28b8d92
Steve Jeong <jst0930@gmail.com>
2020-09-05 14:41:36 +0900

    refactoring

--- main.js
+++ main.js
@@ -2,6 +2,39 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url')
 
+function templateList(filelist) {
+  var contents = ``;
+  for(var list of filelist) {
+    contents += `<li><a href ="/?id=${list}">${list}</a></li>`
+  }
+
+  return contents;
+}
+
+function templateHTML(title, contents, description) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    <ul>${contents}</ul>
+    <h2>${title}</h2>
+    <p>${description}</p>
+  </body>
+  </html>
+`;
+}
+
+function makePage(filelist, title, contents, description, response) {
+  template = templateHTML(title, contents, description);
+  response.writeHead(200);
+  response.end(template);
+}
+
 var app = http.createServer(function (request, response) {
   var _url = url.parse(request.url, true);
   var title = _url.query.id;
@@ -11,74 +44,19 @@ var app = http.createServer(function (request, response) {
       title = 'Welcome';
       description = 'Hello NodeJS';
       fs.readdir('data', 'utf8', (err, filelist) => {
-        var contents = ``;
-        // filelist.forEach(list => {
-        //   contents += `<li><a href ="/?id=${list}">${list}</a></li>`
-        // })
-
-        // for(var list in filelist) {
-        //   contents += `<li><a href ="/?id=${filelist[list]}">${filelist[list]}</a></li>`
-        // }
-
-        for(var list of filelist) {
-          contents += `<li><a href ="/?id=${list}">${list}</a></li>`
-        }
-        
-        template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>${contents}</ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
+        var contents = templateList(filelist);
+        makePage(filelist, title, contents, description, response)
       })
 
     } else {
       fs.readdir('data', 'utf8', (err, filelist) => {
-        var contents = ``;
-        // filelist.forEach(list => {
-        //   contents += `<li><a href ="/?id=${list}">${list}</a></li>`
-        // })
-
-        // for(var list in filelist) {
-        //   contents += `<li><a href ="/?id=${filelist[list]}">${filelist[list]}</a></li>`
-        // }
-
-        for(var list of filelist) {
-          contents += `<li><a href ="/?id=${list}">${list}</a></li>`
-        }
-        
         fs.readFile(`data/${title}`, 'utf8', (err, description) => {
-          template = `
-            <!doctype html>
-            <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              <ul>${contents}</ul>
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-            </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
+          var contents = templateList(filelist);
+          makePage(filelist, title, contents, description, response)
         })
       })
     }
+
   } else {
     response.writeHead(404);
     response.end('<h1><a href="/">WEB</a></h1><p>Page Not Found.</p>');
